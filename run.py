4976b9b3cf08a64819229c7430f08b9a02ba8811
sakshammaggu <sakshammaggu80@gmail.com>
2025-01-12 14:47:29 +0530

    update host

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ load_dotenv()
 server_port = int(os.getenv("PORT", 8001))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=server_port, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=server_port, reload=True)
